efc7fa159ecbce076c037a7659901c7b7c881a20
Yuhai Shi <shi@uleth.ca>
2016-03-21 18:22:45 -0600

    new change

--- Player.cc
+++ Player.cc
@@ -13,10 +13,10 @@ Player::~Player()
 void Player::doLogic(Keyboard keyboard)
 {
     if (keyboard.key[UP]&&y>70)
-       y -= moveSpeed;
+        y -= moveSpeed;
     else if (keyboard.key[DOWN]&&y<490)
         y += moveSpeed;
-    if (keyboard.key[LEFT]&&x>0)
+    if (keyboard.key[LEFT]&&x>10)
         x -= moveSpeed;
     else if (keyboard.key[RIGHT]&&x<610)
         x += moveSpeed;
