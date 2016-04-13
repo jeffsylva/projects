38d84a5cd1467a397c95b28be801a4dffb0c7c9b
Jefferson Sylva-Iriogbe <j.sylvairiogbe@uleth.ca>
2016-04-13 15:29:19 -0600

    added doxygen documentation to Player.h

--- Player.h
+++ Player.h
@@ -8,15 +8,39 @@
 class Player : public bullet
 {
   private:
-   int health, moveSpeed,bMoveSpeed;
+   int health, 
+   moveSpeed,  /**< Rate at which the player can be moved*/
+   bMoveSpeed; /**<speed of the bullet*/
    
   public:
+  	/** @fn Player()
+    * @brief Constructor
+    * @param No parameters
+    * @return No return value
+    */
+
    Player();
+         /** @fn ~Player()
+    * @brief Destructor
+    * @param No parameters
+    * @return No return value
+    */
+
    ~Player();
-   
+   /** @fn Void doLogic(Keyboard keyboard)
+    * @brief Defines the movement key controls for the player object so it can be moved using a keyboard
+    * @param This function takes in a Keyboard object
+    * @return No return value
+    */
    void doLogic(Keyboard keyboard);
-void doSpaceL(Keyboard keyboard);
-void moveBullet();
+
+   /** @fn void moveBullet()
+    * @brief This function initializes the movement of the bullet through the display 
+    * @param This function takes no parameters
+    * @return No return value
+    */
+
+   void moveBullet();
 
 };
 
