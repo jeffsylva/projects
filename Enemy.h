bb59021476e386958a41afb89e812c02b371fa42
Jefferson Sylva-Iriogbe <j.sylvairiogbe@uleth.ca>
2016-04-13 15:26:19 -0600

    added doxygen documentation to Enemy.h

--- Enemy.h
+++ Enemy.h
@@ -6,14 +6,36 @@
 class Enemy:public EnemyOne
 {
   protected:
-   int health;
+   int health; /**< the health of the enemy which is set to 0 by the constructor */
 
   public:
+  	/** @fn Enemy()
+    * @brief Constructor
+    * @param No parameters
+    * @return No return value
+    */
    Enemy();
+
+   /** @fn ~Enemy()
+    * @brief Destructor
+    * @param No parameters
+    * @return No return value
+    */
    ~Enemy();
 
+   /** @fn void moveEnemy()
+    * @brief Defines the movement of the first enemy type to move from right to left by subtracting the 'moveSpeed' from the x coordinate. if enemy goes out of the screen on the left, it is reset to the right side.
+    * @param No parameters
+    * @return No return value
+    */
    void moveEnemy();
-void moveboss();
+
+   /** @fn void moveboss()
+    * @brief Defines the movement of the second enemy type to move from right to left by subracting the 'moveSpeed' from the x coordinate.
+    * @param No parameters
+    * @return No return value
+    */
+	void moveboss();
 };
 
 #endif
