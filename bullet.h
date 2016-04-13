00dfacdc14e7e39c6e14898707c04e15c024bb11
Jefferson Sylva-Iriogbe <j.sylvairiogbe@uleth.ca>
2016-04-13 15:22:28 -0600

    added doxygen documentation to bullet.h

--- bullet.h
+++ bullet.h
@@ -10,28 +10,51 @@
 using namespace std;
 class bulletType{
 private:
-int bX;
-int bY;
+int bX; /**<X coordinate of the bullet*/
+int bY; /**<Y coordinate of the bullet*/
 public:
+  /** @fn bulletType(int a, int b)
+    * @brief Constructor  
+    * @param a X coordinate
+    * @param b Y coordinate
+    * @return No return value
+    */
 bulletType(int a,int b){
-bX=a;
-bY=b;
-}
-int getbX()
-{
-return bX;
+    bX=a;
+    bY=b;
 }
 
+/** @fn int getbX()
+    * @brief Gets the x coordinate of the bullet
+    * @param No parameters  
+    * @return Integer value
+    */
+int getbX(){
+      return bX;
+  }
+/** @fn void setbY(int a)
+    * @brief Sets the Y coordinate of the bullet to the value of the int parameter
+    * @param a : int value of the new Y coordinate of the bullet
+    * @return No return value
+    */
 void setbY(int a){
    bY=a;
  
 }
-
+/** @fn void setbX(int b)
+    * @brief Sets the X coordinate of the bullet to the value of the int parameter
+    * @param b : int value of the new X coordinate of the bullet
+    * @return No return value
+    */
 void setbX(int b){
    bX=b;
  
 }
-
+/** @fn int getbY
+    * @brief Gets the y coordinate of the bullet
+    * @param No parameters
+    * @return Integer value
+    */
 int getbY()
 {
    return bY;
@@ -40,21 +63,54 @@ int getbY()
 
 
 
-
-
-
 class bullet:public Plane
 {
 protected:
-   ALLEGRO_BITMAP *Bbitmap;
+   ALLEGRO_BITMAP *Bbitmap; /**<Creates a Bitmap object that will hold the bullet image*/
   public:
-list<bulletType*> Blist;
+list<bulletType*> Blist; /**<Creates a list of the bullets*/
+   
+   /** @fn bullet()
+    * @brief Constructor  
+    * @param No parameters
+    * @return No return value
+  */
    bullet();
-  ~bullet();
-void addBullet(int,int);
-  void setBullet(std::string fileName);
-  ALLEGRO_BITMAP *getBulletMap();
 
+   /** @fn ~bullet()
+    * @brief Destructor 
+    * @param No parameters  
+    * @return No return value
+    */
+  ~bullet();
+
+  /** @fn void addBullet(int,int)
+    * @brief Creates the bullet and sets it at the x and y coordinates given with the two int parameters
+    * @param int : representing the x coordinate of the bullet
+    * @param int : representing the y coordinate of the bullet
+    * @return No return value
+    */
+void addBullet(int,int);
+
+/** @fn setBullet(std::string fileName)
+    * @brief Loads the bitmap image of the bullet
+    * @param fileName : the file name/path that contains the image of the bullet
+    * @return No return value
+    */
+  void setBullet(std::string fileName);
+
+  /** @fn ALLEGRO_BITMAP *getBulletMap()
+    * @brief getter function to get the bitmat image of the bullet
+    * @param No parameters
+    * @return Bitmap image of the bullet
+    */
+  ALLEGRO_BITMAP *getBulletMap();
+  
+  /** @fn drawB()
+    * @brief Draw function that actually draws the bullet image
+    * @param No parameters
+    * @return No return value
+    */
   void drawB();
 
 };
