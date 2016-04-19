ec93f502acc22c1581a186bcb9f16715ba383c17
Jefferson Sylva-Iriogbe <j.sylvairiogbe@uleth.ca>
2016-04-19 05:01:49 -0600

    updated doxygen on Keyboard.h

--- Keyboard.h
+++ Keyboard.h
@@ -1,3 +1,12 @@
+/**@file Keyboard.h
+*  @brief Definition of the Keyboard Class.
+*  
+*  This contains the public and private member variables 
+*  and functions of the Keyboard class
+*  @author Wang Kangning, Jefferson Sylva-Iriogbe and Yuhai Shi.
+*  @bug No known bugs.
+*/
+
 #ifndef KEYBOARD_H_
 #define KEYBOARD_H_
 
